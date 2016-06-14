d169cbe375554d6864ee1630900844f7ef42aed4
pjford84 <Ifrit@Patricks-MacBook-Pro.local>
2016-06-14 11:00:31 -0700

    pf changes

--- patrick-codes.R
+++ patrick-codes.R
@@ -1,17 +1,25 @@
 #patrick-codes.R
+library(dplyr)
+library(lubridate)
 
 #make fake data
-n <- 30
-clientData <- data.frame(name = sample(c("josh", "patrick", "obama", "trump", "grinch", "hippo"), n, replace=TRUE), recordId = 1:n, code = sample(LETTERS,n, replace=TRUE), value=sample(10:1000, n, replace=TRUE))
+#n <- 30
+#clientData <- data.frame(name = sample(c("josh", "patrick", "obama", "trump", "grinch", "hippo"), n, replace=TRUE), recordId = 1:n, code = sample(LETTERS,n, replace=TRUE), value=sample(10:1000, n, replace=TRUE))
 
 #get unique list of clients
-clientList <- unique(clientData$name) %>% as.character()
+#clientList <- clientData$name %>% unique() %>% as.character()
+
+#import csv
+
 
 #for loop solution
 for (x in 1:length(clientList)) {
   client <- clientList[x]
   subsetData <- subset(clientData, clientData$name==client)
-  write.csv(subsetData, paste("./",client,".csv", sep=""))
+  
+  clientData[clientData$name==client,c("name","value")]
+  
+  write.csv(subsetData, paste("./timelogs/",client,".csv", sep=""))
 }
 
 #solution usign apply()
