869a3c983da06e3a33193af1b31982270b3abb49
pjford84 <Ifrit@Patricks-MacBook-Pro.local>
2016-06-22 09:12:04 -0700

    more pf changes

--- patrick-codes.R
+++ patrick-codes.R
@@ -1,31 +1,26 @@
 #patrick-codes.R
 library(dplyr)
 library(lubridate)
+setwd("/Users/Ifrit/Documents/codes/patrickCodes/")
 
-#make fake data
-#n <- 30
-#clientData <- data.frame(name = sample(c("josh", "patrick", "obama", "trump", "grinch", "hippo"), n, replace=TRUE), recordId = 1:n, code = sample(LETTERS,n, replace=TRUE), value=sample(10:1000, n, replace=TRUE))
-
-#get unique list of clients
-#clientList <- clientData$name %>% unique() %>% as.character()
 
 #import csv
+mytable <- read.csv("./clients14.csv", stringsAsFactors = FALSE)
 
+#get unique list of clients
+clientList <- mytable$Client %>% unique() %>% as.character() %>% sort()
 
 #for loop solution
 for (x in 1:length(clientList)) {
-  client <- clientList[x]
-  subsetData <- subset(clientData, clientData$name==client)
-  
-  clientData[clientData$name==client,c("name","value")]
-  
-  write.csv(subsetData, paste("./timelogs/",client,".csv", sep=""))
+  uclient <- clientList[x]
+  subsetData <- subset(mytable, mytable$Client==clientList)
+  write.csv(subsetData, paste("./timelogs/",uclient,".csv", sep=""))
 }
 
 #solution usign apply()
-f1 <- function(client, y) {
-  subsetData <- subset(y, y$name==client)
-  write.csv(subsetData, file=paste("./",client,".csv", sep=""))
-}
-
-lapply(clientList, f1, y=clientData)
+#f1 <- function(client, y) {
+#  subsetData <- subset(y, y$name==client)
+#  write.csv(subsetData, file=paste("./",client,".csv", sep=""))
+#}
+#
+#lapply(clientList, f1, y=clientData)
